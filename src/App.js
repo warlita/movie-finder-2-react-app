8b94074b86893ec54b368e1e895d9ceb20ef95d4
Analita <88949609+warlita@users.noreply.github.com>
2024-04-01 17:44:28 -0500

    movie finder 2 which is movie finder as a react app

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import Home from './home';
+import Movie from './movie';
+
+import './App.css';
+
+const NotFound = () => {
+  return <h2>404 Not Found</h2>;
+}
+
+const App = () => {
+  return (
+    <Router>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <Link className="navbar-brand" to="/">Movie Finder</Link>
+      </nav>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route component={NotFound} />
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
