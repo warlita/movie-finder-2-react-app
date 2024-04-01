e1516c4ecb7f18135ce5f323fbe705c155a0cd89
Analita <88949609+warlita@users.noreply.github.com>
2024-04-01 18:03:20 -0500

    setting up to deploy on GH pages

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ const NotFound = () => {
 
 const App = () => {
   return (
-    <Router>
+    <Router basename="/first-react-app">
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <Link className="navbar-brand" to="/">Movie Finder</Link>
       </nav>
